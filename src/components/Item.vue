d9221ecc3c97727a3b5fae7e915a99bf1a23fba4
jiahao.feng <445786355@qq.com>
2020-10-20 18:51:04 +0800

    武器属性适配

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="m-item" v-if="item">
     <div class="item-detail">
+      <!-- 精炼等级 -->
+      <div v-if="item.MaxStrengthLevel" class="u-max-strength-level">
+        <span v-text="`精炼等级：0 / ${item.MaxStrengthLevel}`"></span>
+      </div>
       <!-- 物品名称 -->
       <h4 class="u-title" :style="{color:$options.filters.item_color(item.Quality)}" v-text="item.Name"></h4>
       <!-- 绑定状态 -->
@@ -9,16 +13,23 @@
       <div v-if="parseInt(item.MaxExistAmount) === 1" class="unique" v-text="'唯一'"></div>
       <!-- 装备属性 -->
       <div class="m-attributes" v-if="item.attributes.length">
-        <div v-for="(attribute,key) in item.attributes" :key="key" class="m-field">
+        <div v-for="(attribute,key) in item.attributes" :key="key" class="m-field" :class="[`u-${attribute.color}`]">
           <span v-if="attribute.type=='atMeleeWeaponAttackSpeedBase'" class="u-value u-speed"
-                :style="{color:attribute.color}" v-text="attribute.label"></span>
-          <span v-else class="u-value" :style="{color:attribute.color}" v-text="attribute.label"></span>
+                v-text="attribute.label"></span>
+          <span v-else class="u-value" v-text="attribute.label"></span>
         </div>
       </div>
-      <ul v-if="item.Diamonds" class="m-diamonds">
+      <!-- 镶嵌 -->
+      <ul v-if="item.Diamonds" class="m-diamonds u-gray">
+        <!-- 五行石 -->
         <li class="m-diamond" v-for="(label,key) in item.Diamonds" :key="key">
           <span class="u-square"></span>
           <span class="u-text" v-text="`镶嵌孔：${label}`"></span>
+        </li>
+        <!-- 五彩石 -->
+        <li v-if="item.AucGenre == 1" class="m-diamond">
+          <span class="u-square"></span>
+          <span class="u-text">&lt;只能镶嵌五彩石&gt;</span>
         </li>
       </ul>
       <!-- 仅性别可穿戴 -->
@@ -30,11 +41,13 @@
       <!-- 最大耐久度 -->
       <div v-if="item.MaxDurability" class="u-max-durability" v-text="'最大耐久度' + item.MaxDurability"></div>
       <!-- 描述 -->
-      <p v-if="item.DescHtml" class="u-desc" v-html="item.DescHtml"></p>
+      <p v-if="item.DescHtml" class="u-desc u-yellow" v-html="item.DescHtml"></p>
       <!-- 品质等级 -->
-      <div v-if="item.Level" class="u-level" v-text="'品质等级' + item.Level"></div>
+      <div v-if="item.Level" class="u-level u-yellow" v-text="'品质等级' + item.Level"></div>
       <!-- 装备分数 -->
-      <div v-if="item.EquipmentRating" class="u-equipment-rating" v-text="'装备分数' + item.EquipmentRating"></div>
+      <div v-if="item.EquipmentRating" class="u-equipment-rating u-orange" v-text="'装备分数' + item.EquipmentRating"></div>
+      <!-- 推荐门派心法 -->
+      <div v-if="item.Recommend" class="u-equipment-recommend" v-text="'推荐门派：' + item.Recommend"></div>
     </div>
   </div>
 </template>
--- src/filters/ItemColor.js
+++ src/filters/ItemColor.js
@@ -10,13 +10,13 @@ export default (quality) => {
         case 1:
             return 'rgb(255,255,255)';
         case 2:
-            return 'rgb(0,209,75)';
+            return 'rgb(0,210,75)';
         case 3 :
-            return 'rgb(0,126,254)';
+            return 'rgb(0,126,255)';
         case 4:
             return 'rgb(254,45,254)';
         case 5:
-            return 'rgb(249,161,1)';
+            return 'rgb(255,165,0)';
         default:
             return 'rgb(0,0,0)'
     }
