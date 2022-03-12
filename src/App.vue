327b6b2d8c3a0f4cc63ad4b5707e294eb7fe3847
zixuan <litthy@qq.com>
2022-03-12 13:43:10 +0800

    fix：keep alive添加 #12

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-    <div id="app">
-        <Header></Header>
-        <!-- 频道栏 -->
-        <Breadcrumb
-            name="物品百科"
-            slug="item"
-            root="/item"
-            :publishEnable="true"
-            :feedbackEnable="true"
-            :adminEnable="false"
-        >
-            <img slot="logo" svg-inline :src="getAppIcon('item')" />
-            <ItemBreadcrumb />
-        </Breadcrumb>
-        <!-- 左侧菜单 -->
-        <LeftSidebar>
-            <Sidebar :sidebar="globalSidebar" />
-        </LeftSidebar>
-        <Main :withoutRight="false">
-            <!-- 路由页面内容 -->
-            <router-view />
-            <!-- 右侧咨询 -->
-            <RightSidebar>
-                <Extend />
-            </RightSidebar>
-            <Footer></Footer>
-        </Main>
-    </div>
+	<div id="app">
+		<Header></Header>
+		<!-- 频道栏 -->
+		<Breadcrumb name="物品百科" slug="item" root="/item" :publishEnable="true" :feedbackEnable="true" :adminEnable="false">
+			<img slot="logo" svg-inline :src="getAppIcon('item')" />
+			<ItemBreadcrumb />
+		</Breadcrumb>
+		<!-- 左侧菜单 -->
+		<LeftSidebar>
+			<Sidebar :sidebar="globalSidebar" />
+		</LeftSidebar>
+		<Main :withoutRight="false">
+			<!-- 路由页面内容 -->
+			<!-- <router-view /> -->
+			<keep-alive>
+				<router-view v-if="$route.meta.keepAlive" />
+			</keep-alive>
+			<router-view v-if="!$route.meta.keepAlive" />
+			<!-- 右侧咨询 -->
+			<RightSidebar>
+				<Extend />
+			</RightSidebar>
+			<Footer></Footer>
+		</Main>
+	</div>
 </template>
 
 <script>
@@ -35,60 +32,54 @@ import Sidebar from "@/components/Sidebar.vue";
 import Extend from "@/components/Extend.vue";
 import { getAppIcon } from "@jx3box/jx3box-common/js/utils";
 export default {
-    name: "App",
-    props: [],
-    data: function () {
-        return {};
-    },
-    computed: {
-        globalSidebar() {
-            return this.$store.state.sidebar;
-        },
-        query() {
-            return this.$route.query;
-        },
-        params() {
-            return this.$route.params;
-        },
-    },
-    methods: {
-        getAppIcon,
-    },
-    mounted: function () {},
-    components: {
-        ItemBreadcrumb,
-        Sidebar,
-        Extend,
-    },
-    watch: {
-        $route: {
-            // immediate: true,
-            handler() {
-                let sidebar = {}
-                if (
-                    typeof this.params.AucGenre === "undefined" &&
-                    typeof this.params.AucSubTypeID === "undefined" &&
-                    typeof this.query.auc_genre === "undefined" &&
-                    typeof this.query.auc_sub_type_id === "undefined" &&
-                    !this.params.item_id
-                ) {
-                    sidebar = { AucGenre: null, AucSubTypeID: null }
-                } else {
-                    sidebar = {
-                        AucGenre: this.query.auc_genre || '',
-                        AucSubTypeID: this.query.auc_sub_type_id || ''
-                    }
-                    // 如存在路由参数，优先使用路由参数
-                    if (this.params.AucGenre) {
-                        sidebar = {
-                            AucGenre: this.params.AucGenre === "empty" ? '' : this.params.AucGenre,
-                            AucSubTypeID: this.params.AucSubTypeID || ''
-                        }
-                    }
-                }
-                this.$store.commit('SET_STATE', { key: 'sidebar', value: sidebar })
-            },
-        },
-    },
+	name: "App",
+	props: [],
+	data: function () {
+		return {};
+	},
+	computed: {
+		globalSidebar() {
+			return this.$store.state.sidebar;
+		},
+		query() {
+			return this.$route.query;
+		},
+		params() {
+			return this.$route.params;
+		},
+	},
+	methods: {
+		getAppIcon,
+	},
+	mounted: function () {},
+	components: {
+		ItemBreadcrumb,
+		Sidebar,
+		Extend,
+	},
+	watch: {
+		$route: {
+			// immediate: true,
+			handler() {
+				let sidebar = {};
+				if (typeof this.params.AucGenre === "undefined" && typeof this.params.AucSubTypeID === "undefined" && typeof this.query.auc_genre === "undefined" && typeof this.query.auc_sub_type_id === "undefined" && !this.params.item_id) {
+					sidebar = { AucGenre: null, AucSubTypeID: null };
+				} else {
+					sidebar = {
+						AucGenre: this.query.auc_genre || "",
+						AucSubTypeID: this.query.auc_sub_type_id || "",
+					};
+					// 如存在路由参数，优先使用路由参数
+					if (this.params.AucGenre) {
+						sidebar = {
+							AucGenre: this.params.AucGenre === "empty" ? "" : this.params.AucGenre,
+							AucSubTypeID: this.params.AucSubTypeID || "",
+						};
+					}
+				}
+				this.$store.commit("SET_STATE", { key: "sidebar", value: sidebar });
+			},
+		},
+	},
 };
 </script>
